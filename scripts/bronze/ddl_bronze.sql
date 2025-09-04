483b8bcb743e3f162e054378a9da225df269a6d9
tawhidtbs <tawhid.tbs1@gmail.com>
2025-09-04 10:12:46 +0400

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -46,7 +46,7 @@ IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.crm_sales_details(
-  sls_ord       NVARCHAR(50),
+  sls_ord_num       NVARCHAR(50),
   sls_prd_key   NVARCHAR(50),
   sls_cust_id   INT,
   sls_order_dt  INT,
