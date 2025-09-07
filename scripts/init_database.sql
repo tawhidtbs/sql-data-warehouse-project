229d2a2eca925d1ae6c265631390e011f1a672bb
tawhidtbs <tawhid.tbs1@gmail.com>
2025-09-07 07:12:46 +0400

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,7 +1,7 @@
 /*
-========================================================
+=================================================================================================
 Create Database and Schemas
-========================================================
+=================================================================================================
 Script Purpose:
 	This script creates a new database named 'DataWarehouse' after checking if it already exists.
 	If the databas exists, it is dropped and recreated. Additionally, the script sets up three 
@@ -11,12 +11,16 @@ WARNING:
 	Running this script will drop the entire 'DataWarehouse' database if it exists.
 	All data in the database will be permanently deleted. Proceed with caution and
 	ensure you have proper backups before running the script.
+=================================================================================================
 */
 
 USE master;
 GO
 
+--=========================================================================
 -- Drop and Recreate the 'DataWarehouse' database
+--=========================================================================
+	
 IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
 	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
@@ -24,13 +28,19 @@ BEGIN
 END;
 GO
 
+--=========================================================================
 -- Create the 'DataWarehouse' database
+--=========================================================================
+	
 CREATE DATABASE DataWarehouse;
 GO
 USE DataWarehouse;
 GO
 
+--=========================================================================
 -- Create Schemas
+--=========================================================================
+	
 CREATE SCHEMA bronze;
 GO
 CREATE SCHEMA silver;
